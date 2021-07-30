652d3cd9b817465e0ac91ed52dff49dfb57697c7
Joao Pedro <jp.ssilva@live.com>
2021-07-30 10:09:20 -0300

    Commit inicial

---
+++ src/main/java/com/dio/Sistema/SistemaDeControleDeAcessoApplication.java
@@ -0,0 +1,13 @@
+package com.dio.Sistema.de.Controle.de.Acesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeControleDeAcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeControleDeAcessoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/Sistema/de/Controle/de/Acesso/SistemaDeControleDeAcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.Sistema.de.Controle.de.Acesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeControleDeAcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
